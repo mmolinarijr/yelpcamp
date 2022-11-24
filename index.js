a6a606031f03c7624bb05c02eaa464dd3ac33e4b
Mauricio Molinari <mmolinarijr@gmail.com>
2022-11-24 19:47:23 -0300

    setup moongose

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 import express from 'express';
-// import views from 'views';
-import path from 'path';
+import mongoose from 'mongoose';
+import path, { dirname } from 'path';
+import * as dotenv from 'dotenv';
 
-const app = express;
+const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURL, {
+    useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+    console.log('database connected successfully');
+});
+
+const dirnamePath = path.resolve();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(dirnamePath, 'views'));
 
-app.listen(300, () => {
+app.listen(3000, () => {
     console.log('Serving on port 3000');
 });
 
 app.get('/', (req, res) => {
     res.render('home');
-    res.send('This is the boilerplate');
+    // res.send('This is the boilerplate');
 });
+
+export default app;
